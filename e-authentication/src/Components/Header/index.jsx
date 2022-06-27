5cdde2d60f8469376703801f36b93abfb6309709
Nain08 <naindeepkaur08@gmail.com>
2022-06-27 23:13:08 +0530

    Responsiveness updated

--- e-authentication/src/Components/Header/index.jsx
+++ e-authentication/src/Components/Header/index.jsx
@@ -1,34 +1,63 @@
-import React from 'react'
-import './styles.css'
+import React, { useState } from "react";
+import "./styles.css";
+import icon from './icon.png'
 function Header() {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
   return (
     <div>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"></link>
-        <nav className="navbar navbar-expand-lg bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Authenticator</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul className="navbar-nav">
-                            
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/login">Login</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/signup">Sign Up</a>
-                            </li>
-                            
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+        crossorigin="anonymous"
+      ></link>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <img src={icon} alt="icon" className="icon" />
+            Authenticator
+          </a>
+          <div>
+          <button
+            className="custom-toggler navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarsExample09"
+            aria-controls="navbarsExample09"
+            aria-expanded={!isNavCollapsed ? true : false}
+            aria-label="Toggle navigation"
+            onClick={handleNavCollapse}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/login">
+                  Login
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/signup">
+                  Sign Up
+                </a>
+              </li>
+            </ul>
+          </div>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- e-authentication/src/Components/Home/index.js
+++ e-authentication/src/Components/Home/index.js
@@ -11,9 +11,7 @@ function Home() {
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"></link>
             <div>
                 <img className="bg" src={background} alt="Background" />
-            </div>
-
-            <div className='heading'>
+                <div className='heading'>
                 EAT.
                 <br />
                 SLEEP.
@@ -34,6 +32,9 @@ function Home() {
                 </div>
             </Link>
 
+            </div>
+
+            
 
         </>
     )
